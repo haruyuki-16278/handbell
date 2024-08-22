2c45912a4a1b4c37e322fe0a6d3e720549762839
haruyuki@chrome-penguin <haruyuki@relicsnow.info>
2024-08-23 02:42:16 +0900

    👍 演奏用のルームを作成できるように機能を追加

--- server.deno.ts
+++ server.deno.ts
@@ -4,16 +4,17 @@ import { messages } from "./shared/constant.ts";
 const kv = await Deno.openKv();
 
 Deno.serve(async (req) => {
-  const pathname = new URL(req.url).pathname;
+  const url = new URL(req.url);
+  const pathname = url.pathname;
   console.log(pathname);
-  
+
   if (pathname.startsWith("/ws") && req.method === "GET") {
     if (req.headers.get("upgrade") === "websocket") {
-      const {socket, response} = Deno.upgradeWebSocket(req);
+      const { socket, response } = Deno.upgradeWebSocket(req);
 
       socket.onopen = () => {
         console.log("WebSocket connection opened");
-      }
+      };
       socket.onmessage = async (message) => {
         if (message.data.includes(messages.status)) {
           const status = await kv.get(["status"]);
@@ -23,13 +24,13 @@ Deno.serve(async (req) => {
           const next = await kv.get(["next"]);
           socket.send(String(next.value));
         }
-      }
+      };
       socket.onclose = () => {
         console.log("WebSocket connection closed");
-      }
+      };
       socket.onerror = (error) => {
         console.error("WebSocket error:", error);
-      } 
+      };
 
       return response;
     }
@@ -37,6 +38,41 @@ Deno.serve(async (req) => {
 
   if (req.method === "GET" && pathname === "/welcome-message") {
     return new Response("jigインターンへようこそ！");
+  }
+
+  if (req.method === "GET" && pathname === "/rooms") {
+    const iterable = await kv.list<Record<string, string>>({
+      prefix: ["room"],
+    });
+    const rooms = [];
+    for await (const room of iterable) {
+      console.log(room)
+      if (new Date().getTime() - new Date(room.value.createdAt).getTime() < 1000 * 60 * 5) {
+        rooms.push({...room.value, id: room.key[1]});
+      }
+    }
+    return new Response(JSON.stringify(rooms));
+  }
+  
+  if (req.method === "POST" && pathname === "/rooms") {
+    const data = await req.json();
+    const id = crypto.randomUUID();
+    await kv.set(["room", id], {
+      name: data.name,
+      owner: data.owner,
+      createdAt: new Date().toISOString(),
+    });
+    return new Response(JSON.stringify({name: data.name, id: id, owner: data.owner}));
+  }
+  
+  if (req.method === "DELETE" && pathname === "/rooms") {
+    const id = url.searchParams.get("id");
+    
+    if (!id) {
+      return new Response("id is required", { status: 400 });
+    }
+    await kv.delete(["room", id]);
+    return new Response("deleted");
   }
 
   return serveDir(req, {
